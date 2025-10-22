8126cd725d0f3854f829c352c70870931da99122
Carlos <carlosrodrigoese@gmail.com>
2025-10-22 00:10:01 -0600

    Se creo DbContext y Modelo Alumno

---
+++ Data/Models/Alumno.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace REPO_TUT.Data.Models
+{
+    public class Alumno
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; } = string.Empty;
+        [Required(ErrorMessage = "La matrícula es obligatoria")]
+        public string Matricula { get; set; } = string.Empty;
+        [Range(1, 12, ErrorMessage = "El cuatrimestre debe estar entre 1 y 12")]
+        public int Cuatrimestre { get; set; }
+    }
+}
---
+++ Data/REPOTUTDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using REPO_TUT.Data.Models;
+using System;
+
+namespace REPO_TUT.Data
+{
+    public class REPOTUTDbContext : DbContext
+    {
+        public REPOTUTDbContext(DbContextOptions<REPOTUTDbContext> options) : base(options) { }
+
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
